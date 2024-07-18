bee0dc40e5f69a89234ebe190606b220a90164c9
rafaelrguerra <rafarguerra@gmail.com>
2024-07-18 12:27:16 -0300

    Update 20240717214731_initial.cs

--- HealthCheck.Auth/HealthCheck.Auth.Infrastructure/Migrations/20240717214731_initial.cs
+++ HealthCheck.Auth/HealthCheck.Auth.Infrastructure/Migrations/20240717214731_initial.cs
@@ -37,10 +37,10 @@ namespace HealthCheck.Auth.Infrastructure.Migrations
                 columns: new[] { "Id", "Cpf", "CreatedAt", "Crm", "Email", "Name", "PasswordHash", "UpdatedAt", "UserRole" },
                 values: new object[,]
                 {
-                    { new Guid("8d2f05bb-f651-4b26-855f-fcc445746d52"), "33333333333", new DateTime(2024, 7, 17, 21, 47, 31, 41, DateTimeKind.Utc).AddTicks(7504), null, "patient2@healthcheck.com", "Patient Two", "", null, 1 },
-                    { new Guid("bf488734-a3f6-48fd-9a44-f493ee2ea028"), "22222222222", new DateTime(2024, 7, 17, 21, 47, 31, 41, DateTimeKind.Utc).AddTicks(7115), null, "patient1@healthcheck.com", "Patient One", "", null, 1 },
-                    { new Guid("edd023dd-90b3-4637-95fe-581d855aeef9"), null, new DateTime(2024, 7, 17, 21, 47, 31, 41, DateTimeKind.Utc).AddTicks(5832), "000000", "doctor1@healthcheck.com", "Doctor One", "", null, 0 },
-                    { new Guid("ffa0a2f0-db68-4fd7-8a39-92b40002fc34"), null, new DateTime(2024, 7, 17, 21, 47, 31, 41, DateTimeKind.Utc).AddTicks(6749), "111111", "doctor2@healthcheck.com", "Doctor Two", "", null, 0 }
+                    { new Guid("8d2f05bb-f651-4b26-855f-fcc445746d52"), "33333333333", new DateTime(2024, 7, 17, 21, 47, 31, 41, DateTimeKind.Utc).AddTicks(7504), null, "patient2@healthcheck.com", "Patient Two", "$2a$11$C99.K9/gfTc0RqR8XYAiu.T3BG/GvWgOt2oggKkyivz9dGpZPwpEy", null, 1 },
+                    { new Guid("bf488734-a3f6-48fd-9a44-f493ee2ea028"), "22222222222", new DateTime(2024, 7, 17, 21, 47, 31, 41, DateTimeKind.Utc).AddTicks(7115), null, "patient1@healthcheck.com", "Patient One", "$2a$11$C99.K9/gfTc0RqR8XYAiu.T3BG/GvWgOt2oggKkyivz9dGpZPwpEy", null, 1 },
+                    { new Guid("edd023dd-90b3-4637-95fe-581d855aeef9"), null, new DateTime(2024, 7, 17, 21, 47, 31, 41, DateTimeKind.Utc).AddTicks(5832), "000000", "doctor1@healthcheck.com", "Doctor One", "$2a$11$C99.K9/gfTc0RqR8XYAiu.T3BG/GvWgOt2oggKkyivz9dGpZPwpEy", null, 0 },
+                    { new Guid("ffa0a2f0-db68-4fd7-8a39-92b40002fc34"), null, new DateTime(2024, 7, 17, 21, 47, 31, 41, DateTimeKind.Utc).AddTicks(6749), "111111", "doctor2@healthcheck.com", "Doctor Two", "$2a$11$C99.K9/gfTc0RqR8XYAiu.T3BG/GvWgOt2oggKkyivz9dGpZPwpEy", null, 0 }
                 });
         }
 
